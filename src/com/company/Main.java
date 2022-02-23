1670c81362067d7dea769eae07cefe55c2ec3392
TorVik247 <promasterbro248@gmail.com>
2022-02-23 13:14:11 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Батько: Полянський ОЛександр Юрійович, 1976 року народження, підприємець;");
+        System.out.print("..." + "\n");
+        System.out.println("МАти: Полянська Христина Іванівна, 1980 року народження , поштарка; ");
+        System.out.print("..." + "\n");
+        System.out.println("Брат: Полянський Артур Олександрович,2006 року народження ,студент ;");
+    }
+}
